4b22ecbfee5eebe76e99de389f518c1bd01b88a3
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2024-03-14 20:04:38 +0200

    Add project files.

---
+++ Mutexes/Program.cs
@@ -0,0 +1,10 @@
+﻿using Mutexes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Reporter reporter = new Reporter();
+        reporter.Start();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Module 4_1
+
+## Mutexes
+
+- [Program.cs](Mutexes/Program.cs)
+
+<img src="images/1.png">
